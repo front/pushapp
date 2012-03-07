48c7ae124fcc0df9340f66cbe6ab1bac078d5969
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-03-07 16:01:33 +0100

    serverside working2

---
+++ public_html/node/index.js
@@ -0,0 +1,12 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/moderate"] = requestHandlers.moderate;
+handle["/receive_postmark_data"] = requestHandlers.receive_postmark_data;
+
+
+
+
+server.start(router.route, handle);
---
+++ public_html/node/server.js
@@ -0,0 +1,39 @@
+var http = require("http");
+var url = require("url");
+
+
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  
+    var postData = "";
+  
+  request.setEncoding("utf8");
+  
+  request.addListener("data", function(postDataChunk) {
+    postData += postDataChunk;
+    //console.log("Received POST data chunk '" + postDataChunk + "'.");
+  });
+  
+  request.addListener("end", function() {
+    if (postData)
+      {
+      console.log("GOT POST DATA");
+      require('./requestHandlers.js').handlePostData(pathname, response, request, postData);
+      }    
+      else{
+        console.log("NO POST DATA");
+        //console.log(request);
+      }
+      
+  });
+}
+
+  http.createServer(onRequest).listen(8080);
+  console.log("Server has started.");
+}
+
+exports.start = start;
